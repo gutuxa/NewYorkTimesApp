5e6904b496a973305ef3570ca64483803071ad2e
Oksana Tugusheva <gutuxa@gmail.com>
2021-05-01 10:33:23 +0300

    Initial Commit

---
+++ NetworkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkApp
+//
+//  Created by Oksana Tugusheva on 01.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
